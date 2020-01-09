eaae470f483c2b2a96c982534ff5e9d814583ce1
yikezawa <y.ikezawa93@gmail.com>
2020-01-09 22:33:06 +0900

    mod

--- oauth/oauth_test.go
+++ oauth/oauth_test.go
@@ -1,13 +1,59 @@
 package oauth
 
 import (
-	"github.com/stretchr/testify/assert"
+	"fmt"
+	"net/http"
+	"os"
 	"testing"
+
+	"github.com/mercadolibre/golang-restclient/rest"
+	"github.com/stretchr/testify/assert"
 )
+
+func TestMain(m *testing.M) {
+	fmt.Println("about to start oauth tests")
+	rest.StartMockupServer()
+	os.Exit(m.Run())
+}
 
 func TestOauthConstants(t *testing.T) {
 	assert.EqualValues(t, headerXPublic, "X-Public")
 	assert.EqualValues(t, headerXClientID, "X-Client-ID")
 	assert.EqualValues(t, headerXCallerID, "X-Caller-ID")
 	assert.EqualValues(t, paramAccessToken, "access_token")
+}
+
+func TestIsPublicNilRequest(t *testing.T) {
+	assert.True(t, IsPublic(nil))
+}
+
+func TestIsPublicNoError(t *testing.T) {
+	request := http.Request{
+		Header: make(http.Header),
+	}
+	assert.False(t, IsPublic(&request))
+
+	request.Header.Add("X-Public", "true")
+	assert.True(t, IsPublic(&request))
+}
+
+func TestGetCallerIDNilRequest(t *testing.T) {
+
+}
+
+func TestGetAccessTokenInvalidRestclientResponse(t *testing.T) {
+	rest.FlushMockups()
+	rest.AddMockups(&rest.Mock{
+		HTTPMethod:   http.MethodGet,
+		URL:          "http://localhost:8080/oauth/access_token/AbC123",
+		ReqBody:      ``,
+		RespHTTPCode: http.StatusNotFound,
+		RespBody:     `{}`,
+	})
+
+	accessToken, err := getAccessToken("AbC123")
+	assert.Nil(t, accessToken)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
+	assert.EqualValues(t, "invalid restclient response when trying to get access token", err.Message)
 }
